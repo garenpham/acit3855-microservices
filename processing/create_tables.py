6044c126827dc25ad1193b4516b619c388ffcb53
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-10-27 17:08:12 -0700

    part 1 lab 7

---
+++ processing/create_tables.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect('stats.sqlite')
+c = conn.cursor()
+
+c.execute('''
+        CREATE TABLE stats
+        (id INTEGER PRIMARY KEY ASC,
+        num_ci_readings INTEGER NOT NULL,
+        num_bc_readings INTEGER NOT NULL,
+        max_numPeople INTEGER,
+        max_numNights INTEGER,
+        last_updated VARCHAR(100) NOT NULL)
+''')
+
+
+conn.commit()
+conn.close()
