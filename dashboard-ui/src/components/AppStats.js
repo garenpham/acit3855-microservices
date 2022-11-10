c3d8a01a7ded2dac6c49eeb908ea5dffc4ccb53a
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-11-10 13:39:32 -0800

    .

--- dashboard-ui/src/components/AppStats.js
+++ dashboard-ui/src/components/AppStats.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import "../App.css";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import '../App.css';
 
 export default function AppStats() {
 	const [isLoaded, setIsLoaded] = useState(false);
@@ -11,7 +12,7 @@ export default function AppStats() {
 			.then((res) => res.json())
 			.then(
 				(result) => {
-					console.log("Received Stats");
+					console.log('Received Stats');
 					setStats(result);
 					setIsLoaded(true);
 				},
@@ -27,36 +28,36 @@ export default function AppStats() {
 	}, [getStats]);
 
 	if (error) {
-		return <div className={"error"}>Error found when fetching from API</div>;
+		return <div className={'error'}>Error found when fetching from API</div>;
 	} else if (isLoaded === false) {
 		return <div>Loading...</div>;
 	} else if (isLoaded === true) {
 		return (
 			<div>
 				<h1>Latest Stats</h1>
-				<table className={"StatsTable"}>
+				<table className={'StatsTable'}>
 					<tbody>
 						<tr>
 							<th>Check In</th>
 							<th>Booking Confirm</th>
 						</tr>
 						<tr>
-							<td># CI: {stats["num_ci_readings"]}</td>
-							<td># BC: {stats["num_bc_readings"]}</td>
+							<td># CI: {stats['num_ci_readings']}</td>
+							<td># BC: {stats['num_bc_readings']}</td>
 						</tr>
 						<tr>
 							<td colspan="2">
-								Max number of people stay: {stats["max_numPeople"]}
+								Max number of people stay: {stats['max_numPeople']}
 							</td>
 						</tr>
 						<tr>
 							<td colspan="2">
-								Max number of nights spend: {stats["max_numNights"]}
+								Max number of nights spend: {stats['max_numNights']}
 							</td>
 						</tr>
 					</tbody>
 				</table>
-				<h3>Last Updated: {stats["last_updated"]}</h3>
+				<h3>Last Updated: {stats['last_updated']}</h3>
 			</div>
 		);
 	}
