c51efbc90fe4fc3da0044390694a89744eb8e688
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-11-11 00:15:13 -0800

    .

---
+++ command.sh
@@ -0,0 +1,12 @@
+docker image rm -f
+docker ps
+docker images
+docker exec -it  /bin/bash
+docker build -t receiver:latest .
+docker build -t storage:latest .
+cd ../storage
+cd ../receiver
+cd ../deployment
+docker compose up -d
+docker compose down
+docker logs
